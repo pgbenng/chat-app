7bd36df2a7c5c2bf9ea3e98f9a3c0b8aa7315355
Ta Thi Phuong Ha <macair@MacAirs-MacBook-Air.local>
2019-09-18 19:11:36 -0700

    final

--- static/chat.js
+++ static/chat.js
@@ -1,41 +1,58 @@
 $(document).ready(function() {
- 
   var scrolled = false;
 
-  setInterval(function() {
+  setTimeout(function() {
     getMessages();
   }, 1000);
 
   function getMessages() {
     $.get("/all-messages", function(data, status) {
-      console.log(data);
       dataArray = JSON.parse(data);
-      console.log(dataArray);
 
       $("#messages").html("");
 
       dataArray.forEach(x => {
         $("#messages").append(
-            `<div><span class="message"> <span class="username">${x[1]}: ${x[2]}</div>`);
+          `<div class="message"><div class="username">${x[1]}</div> <span class="message-text"> ${
+            x[2]
+          }</span></div>`
+        );
       });
+
+      if (!scrolled) {
+        var objDiv = document.getElementById("messages");
+        objDiv.scrollTop = objDiv.scrollHeight + 150;
+        scrolled = true;
+      }
     });
   }
 
-  $("#submit").click(function()  {
-    var username = $("#username").val();
-    var message = $("#message").val();
-    $.post("/message", { username: username, message: message }, function(data) {
-    $("#message").val("");
-    });
+  $("#submit").click(function() {
+    postMessage()
   });
 
-  $(document).on("keypress", function(e)  {
+  $(document).on("keypress", function(e) {
     if (e.which == 13) {
-        var username = $("#username").val();
-        var message = $("#message").val();
-        $.post("/message", { username: username, message: message }, function(data) {
-            $("#message").val("");
-        });
+      postMessage()
+      
     }
   });
+  function postMessage() {
+    var username = $("#username").val();
+      var message = $("#message").val();
+      if (username != "" || message != "") {
+        $.post("/message", { username: username, message: message }, function(
+          data
+        ) {
+          $("#messages").append(
+            `<div class="message"><div class="username">${username}</div> <span class="message-text"> ${message}</span></div>`
+          );
+          var objDiv = document.getElementById("messages");
+          objDiv.scrollTop = objDiv.scrollHeight;
+
+          $("#message").val("");
+        });
+      }
+      
+  }
 });
